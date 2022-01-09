6a22e9bc0fb5d7a621ead1e6fcaa707391657f80
lamh85 <lamh85@gmail.com>
2022-01-08 21:27:51 -0800

    Tentative webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development',
+
+  // IMPORTANT: Sort keys alphabetically
+
+  // Used for live reloading.
+  devServer: {
+    // How the server should find the files.
+    contentBase: './dist',
+    hot: true,
+  },
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
+  },
+  output: {
+    // Generated in memory, not physical file.
+    // Has options to programmatically format filename.
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+}
