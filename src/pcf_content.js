8b90e0a163065efda7e20e4713b2b1f8f5db169a
lamh85 <lamh85@gmail.com>
2022-10-01 15:32:16 -0700

    Style extension. WIP: make login form valid for submission.

--- src/pcf_content.js
+++ src/pcf_content.js
@@ -1,41 +1,35 @@
 import GetElement from './content_script/get_element.js'
 import * as Env from '../.env.json'
 
-console.log('running from content script again again')
-
-const enableSignInButton = (passwordElem) => {
-  passwordElem.focus()
-
-  // const keyboardEvent = document.createEvent('KeyboardEvent')
-  // const initMethod =
-  //   typeof keyboardEvent.initKeyboardEvent !== 'undefined'
-  //     ? 'initKeyboardEvent'
-  //     : 'initKeyEvent'
-
-  // keyboardEvent[initMethod](
-  //   'keydown', // event type: keydown, keyup, keypress
-  //   true, // bubbles
-  //   true, // cancelable
-  //   window, // view: should be window
-  //   false, // ctrlKey
-  //   false, // altKey
-  //   false, // shiftKey
-  //   false, // metaKey
-  //   40, // keyCode: unsigned long - the virtual key code, else 0
-  //   0 // charCode: unsigned long - the Unicode character associated with the depressed key, else 0
-  // )
-  // document.dispatchEvent(keyboardEvent)
-}
-
-chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-  console.log('received this message ----------')
-  console.log(request)
-
+const login = async () => {
   const inputElem = await GetElement('input#username')
   inputElem.value = Env.PCF_USERNAME
+  inputElem.className = 'ng-valid ng-touched ng-dirty'
 
   const passwordElem = await GetElement('input#password')
   passwordElem.value = Env.PCF_PASSWORD
+  passwordElem.className = 'ng-dirty ng-valid ng-touched'
 
-  enableSignInButton(passwordElem)
+  const formElem = await GetElement('form')
+  formElem.className = 'form ng-touched ng-dirty ng-valid'
+
+  const signInElem = await GetElement('button[type="submit"]')
+  signInElem.disabled = false
+  // signInElem.click()
+}
+
+const navigateToTransactions = async () => {
+  const inputElem = await GetElement('input#username')
+
+  document.querySelectorAll('a[href*=transactions]')
+}
+
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+  const titleText = document.querySelector('title').innerText
+
+  if (/login/i.test(titleText)) {
+    await login()
+  } else if (/dashboard/i.test(titleText)) {
+    console.log('dashboard page!')
+  }
 })
