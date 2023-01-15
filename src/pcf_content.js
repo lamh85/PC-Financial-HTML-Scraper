7ea1f8ddf99906ded046a4fef506df4e8c16ebeb
lamh85 <lamh85@gmail.com>
2023-01-15 15:53:41 -0800

    Scrape and parse from PCF webpage.

--- src/pcf_content.js
+++ src/pcf_content.js
@@ -1,18 +1,59 @@
 import GetElement from './content_script/get_element.js'
 import * as Env from '../.env.json'
 
-console.log('pcf_cotent.js loaded')
+// Select the node that will be observed for mutations
+const targetNode = document.getElementsByTagName('body')[0]
 
-console.log('chrome object keys:')
-console.log(Object.keys(chrome))
-console.log(chrome.webRequest)
+// Options for the observer (which mutations to observe)
+const config = { attributes: true, childList: true, subtree: true }
 
-chrome.webRequest.onCompleted.addListener(
-  ({ responseHeaders }) => {
-    console.log(responseHeaders)
-  },
-  { types: ['object', 'image', 'media'] }
-)
+// Callback function to execute when mutations are observed
+const callback = (mutationList, observer) => {
+  for (const mutation of mutationList) {
+    if (mutation.type === 'childList') {
+      console.log('childList changed!!!')
+      // console.log(targetNode.innerHTML?.includes('Transactions'))
+
+      const txTables = document.querySelectorAll('tbody.credit')
+      const postedTable = (txTables || [])[1]
+
+      if (!!postedTable) {
+        console.log('Transaction table changed! ---------')
+
+        const pageLoadTime = Date.now()
+
+        const data = [...postedTable.querySelectorAll('tr')].map(
+          (trElement) => {
+            return {
+              vendor: trElement.querySelector('span.description-text')
+                .innerText,
+              date: trElement.querySelector('span.description-date').innerText,
+              amount: trElement.querySelector('.amount').innerText,
+              category: trElement.querySelector('img[alt]').alt,
+              pageLoadTime,
+            }
+          }
+        )
+
+        console.log(data)
+      }
+    }
+  }
+}
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback)
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config)
+
+// chrome.webRequest is not defined. There's a bug in the Chrome Extension library.
+// chrome.webRequest.onCompleted.addListener(
+//   ({ responseHeaders }) => {
+//     console.log(responseHeaders)
+//   },
+//   { types: ['object', 'image', 'media'] }
+// )
 
 const login = async () => {
   const inputElem = await GetElement('input#username')
